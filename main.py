d8b4fff246a31dfc055af6a2c329cb9dc894c9f9
Kavinkumar-05 <kavinkumarchellamuthu123@gmail.com>
2025-11-10 11:42:54 +0530

    Update header and main Flask app logic

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from forms import CreatePostForm,Registerform,LoginForm,CommentForm
 from functools import wraps
 from flask import abort
 from flask_gravatar import Gravatar
-
+import os
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -50,7 +50,7 @@ def admin_only(f):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -250,4 +250,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
